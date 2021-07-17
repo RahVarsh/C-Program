9d6a6816ed371b605806e644e6396766b05966c6
RahVarsh <86889670+RahVarsh@users.noreply.github.com>
2021-07-18 00:18:46 +0530

    Food.c
    When a user enters any random number from 1 to 5,he gets the name of food item with its price.

---
+++ Food.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int num, size;
+    printf("Enter any number from 1 to 5:\n");
+    scanf("%d", &num);
+    switch(num) 
+    {
+        case 1:
+            printf("Food Item - Pasta\nPrice - Rs 179 ");
+            break;    
+        case 2:    
+            printf("Food Item - Sandwich\nPrice - Rs 149 ");
+            break;
+        case 3:
+            printf("Food Item - Burger\nPrice - Rs 129 ");
+            break;
+        case 4:    
+            printf("Food Item - Pizza\nPrice - Rs 349 ");
+            break;
+        case 5:
+            printf("Food Item - French Fries\nPrice - Rs 179 ");
+            break;
+        default:
+            printf("The Entry is out of range\n");
+            break;
+    }
+    return 0;
+} 
